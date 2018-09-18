2bc0f8144c4b6afc7f5025060eb4df375b36d2f3
Filip Hanik <fhanik@pivotal.io>
2018-09-18 14:11:22 -0700

    Add in SimpleSAMLPhP configurations

--- simplesamlphp-for-cf/metadata/saml20-idp-remote.php
+++ simplesamlphp-for-cf/metadata/saml20-idp-remote.php
@@ -22,13 +22,24 @@
 // 	'certFingerprint'      => 'c9ed4dfb07caf13fc21e0fec1572047eb8a7a4cb'
 // );
 
-$metadata['spring.security.saml.idp.id'] = array(
+$metadata['http://localhost:9092/demo-idp'] = array(
 	'name' => array(
-		'en' => 'Spring Security SAML IDP Sample',
+		'en' => 'Spring Security SAML IDP Sample (localhost:9092)',
 	),
 	'description'          => 'Use account credentials user/password.',
-	'SingleSignOnService'  => 'http://localhost:8081/sample-idp/saml/idp/SSO',
-	'SingleLogoutService'  => 'http://localhost:8081/sample-idp/saml/idp/logout',
-	'certData'      => 'MIIChTCCAe4CCQDo0wjPUK8sMDANBgkqhkiG9w0BAQsFADCBhjELMAkGA1UEBhMCVVMxEzARBgNV BAgMCldhc2hpbmd0b24xEjAQBgNVBAcMCVZhbmNvdXZlcjEdMBsGA1UECgwUU3ByaW5nIFNlY3Vy aXR5IFNBTUwxDDAKBgNVBAsMA2lkcDEhMB8GA1UEAwwYaWRwLnNwcmluZy5zZWN1cml0eS5zYW1s MB4XDTE4MDUxNDE0NTUyMVoXDTI4MDUxMTE0NTUyMVowgYYxCzAJBgNVBAYTAlVTMRMwEQYDVQQI DApXYXNoaW5ndG9uMRIwEAYDVQQHDAlWYW5jb3V2ZXIxHTAbBgNVBAoMFFNwcmluZyBTZWN1cml0 eSBTQU1MMQwwCgYDVQQLDANpZHAxITAfBgNVBAMMGGlkcC5zcHJpbmcuc2VjdXJpdHkuc2FtbDCB nzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEA2EuygAucRBWtYifgEH/ErVUive4dZdqo72Bze4Mb kPuTKLrMCLB6IXxt1p5lu+tr0JxOiRO3KFVOO3D0l+j9zOow4g+JdoMQsjSzA6HtL/D9ZjXP6iUx FCYx+qmnVl3X9ipBD/HVKOBlzIqeXTSa5D17uxPQVxK64UDOI3CyY4cCAwEAATANBgkqhkiG9w0B AQsFAAOBgQAj+6b6dlA6SitTfz44LdnFSW9mYaeimwPP8ZtU7/3EJCzLd5eq7N/0kYPNVclZvB45 I0UMT77AHWrNyScm56MTcEpSuHhJHAqRAgJKbciCTNsFI928EqiWSmu//w0ASBN3bVa8nv8/rafu utCq3RskTkHVZnbT5Xa6ITEZxSncow==',
+	'SingleSignOnService'  => 'http://localhost:9092/demo-idp/saml/idp/SSO/alias/demo-idp',
+	'SingleLogoutService'  => 'http://localhost:9092/demo-idp/saml/idp/logout/alias/demo-idp',
+	'certData'      => 'MIICmTCCAgICCQDzzXUiQ2LfozANBgkqhkiG9w0BAQsFADCBkDELMAkGA1UEBhMCVVMxEzARBgNV
+BAgMCldhc2hpbmd0b24xEjAQBgNVBAcMCVZhbmNvdXZlcjEdMBsGA1UECgwUU3ByaW5nIFNlY3Vy
+aXR5IFNBTUwxETAPBgNVBAsMCHNhbWwtaWRwMSYwJAYDVQQDDB1zYW1sLWlkcC5zcHJpbmcuc2Vj
+dXJpdHkuc2FtbDAeFw0xODA5MTgxNzAxNDBaFw0yODA5MTUxNzAxNDBaMIGQMQswCQYDVQQGEwJV
+UzETMBEGA1UECAwKV2FzaGluZ3RvbjESMBAGA1UEBwwJVmFuY291dmVyMR0wGwYDVQQKDBRTcHJp
+bmcgU2VjdXJpdHkgU0FNTDERMA8GA1UECwwIc2FtbC1pZHAxJjAkBgNVBAMMHXNhbWwtaWRwLnNw
+cmluZy5zZWN1cml0eS5zYW1sMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC7G7DCIgZ0Xz4d
+aexBed5FqCowUPQ0DpLi4BsKE1A2FuPaDkAow4zqJASqXB8kGluLNz7Va6wSwiGZiYSDEFkv9Jr+
+pYQrthlfdIOz8bQ8x9qAy5hEuV1o7uwY8xFvnJ96AmGBMBfXV05P6nhyX9qblkbsxrJHkKituIb7
+3vaF8wIDAQABMA0GCSqGSIb3DQEBCwUAA4GBAKQgIdllQppnDHq6jSeXpKknH+sMCdk0afrLDfDG
+unDYrAKi+fMXKytTxLezLZpZgEytoLKB2cq1PJHHpfVYJzuNKaNnvaeRDho729z2FqnO+JLJpTic
+r0wSWKVdq5T78tLeC5rrhWJnhVGGI2jvkJXpSxI4rfQDVVwvX89tYBGT',
 );
 
--- simplesamlphp-for-cf/metadata/saml20-sp-remote.php
+++ simplesamlphp-for-cf/metadata/saml20-sp-remote.php
@@ -8,129 +8,24 @@
 /*
  * Example simpleSAMLphp SAML 2.0 SP
  */
-$metadata['https://saml2sp.example.org'] = array(
-	'AssertionConsumerService' => 'https://saml2sp.example.org/simplesaml/module.php/saml/sp/saml2-acs.php/default-sp',
-	'SingleLogoutService' => 'https://saml2sp.example.org/simplesaml/module.php/saml/sp/saml2-logout.php/default-sp',
-);
-
-$metadata['cloudfoundry-saml-login'] = array(
-    'AssertionConsumerService' => 'http://localhost:8080/uaa/saml/SSO/alias/cloudfoundry-saml-login',
-    'SingleLogoutService' => 'http://localhost:8080/uaa/saml/SingleLogout/alias/cloudfoundry-saml-login',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
-    'simplesaml.nameidattribute' => 'uid',
-);
-
-$metadata['login.10.244.0.34.xip.io'] = array(
-    'AssertionConsumerService' => 'http://login.10.244.0.34.xip.io/saml/SSO/alias/login.10.244.0.34.xip.io',
-    'SingleLogoutService' => 'http://login.10.244.0.34.xip.io/saml/SSO/alias/login.10.244.0.34.xip.io',
+$metadata['http://localhost:9091/demo-sp'] = array(
+    'AssertionConsumerService' => 'http://localhost:9091/demo-sp/saml/sp/SSO/alias/demo-sp',
+    'SingleLogoutService' => 'http://localhost:9091/demo-sp/saml/sp/logout/alias/demo-sp',
     'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
     'simplesaml.nameidattribute' => 'emailAddress',
+    'certData' => 'MIICgTCCAeoCCQC3dvhia5XvzjANBgkqhkiG9w0BAQsFADCBhDELMAkGA1UEBhMCVVMxEzARBgNV
+BAgMCldhc2hpbmd0b24xEjAQBgNVBAcMCVZhbmNvdXZlcjEdMBsGA1UECgwUU3ByaW5nIFNlY3Vy
+aXR5IFNBTUwxCzAJBgNVBAsMAnNwMSAwHgYDVQQDDBdzcC5zcHJpbmcuc2VjdXJpdHkuc2FtbDAe
+Fw0xODA1MTQxNDQ1MzBaFw0yODA1MTExNDQ1MzBaMIGEMQswCQYDVQQGEwJVUzETMBEGA1UECAwK
+V2FzaGluZ3RvbjESMBAGA1UEBwwJVmFuY291dmVyMR0wGwYDVQQKDBRTcHJpbmcgU2VjdXJpdHkg
+U0FNTDELMAkGA1UECwwCc3AxIDAeBgNVBAMMF3NwLnNwcmluZy5zZWN1cml0eS5zYW1sMIGfMA0G
+CSqGSIb3DQEBAQUAA4GNADCBiQKBgQC2iAUrJXrHaSOWrU95v8GUGVVl5vWrYrNRFtsK5qkhB/nR
+bL08CbqIeD4pkJuIg0LuJdsBuMtYqOnhQSFF5tT36OIdld9SfPA5m8zqPLsCcjWPQ66xoMdReEXN
+9E8s/mZOXn3jkKIqywUxJ+wkS5qoBlvmShwDff+igFlF/fBfpwIDAQABMA0GCSqGSIb3DQEBCwUA
+A4GBACDBjvIpc1/2yZ3TQe29bKif5pr/3NdKz4MWBJ6vjRk7Bs2hbPrM2ajxLbqPx6PRPeTOw5XZ
+grufDj9HmrvKHM2LZTp/cIUpxcNpVRyDA4iVNDc7V3qszaWP9ZIswAYnvmyDL2UHVDLE8xoGz/Ak
+xsRNN9VXNHewjQO605umiAKJ',
+    'assertion.encryption' => FALSE,
+    'nameid.encryption' => FALSE,
 );
 
-$metadata['login.identity.cf-app.com'] = array(
-    'AssertionConsumerService' => 'https://login.identity.cf-app.com/saml/SSO/alias/login.identity.cf-app.com',
-    'SingleLogoutService' => 'https://login.identity.cf-app.com/saml/SingleLogout/alias/login.identity.cf-app.com',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-    'simplesaml.nameidattribute' => 'emailAddress',
-);
-
-$metadata['http://52.1.233.136:8080'] = array(
-    'AssertionConsumerService' => 'http://52.1.233.136:8080/saml/SSO/alias/52.1.233.136',
-    'SingleLogoutService' => 'http://52.1.233.136:8080/saml/SSO/alias/52.1.233.136',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-    'simplesaml.nameidattribute' => 'emailAddress',
-);
-
-$metadata['uaa-bosh'] = array(
-    'AssertionConsumerService' => 'https://uaa.uaa-bosh.cf-app.com:8443/saml/SSO/alias/uaa-bosh',
-    'SingleLogoutService' => 'https://uaa.uaa-bosh.cf-app.com:8443/saml/SSO/alias/uaa-bosh',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-    'simplesaml.nameidattribute' => 'emailAddress',
-);
-
-$metadata['testzone1.login.10.244.0.34.xip.io'] = array(
-    'AssertionConsumerService' => 'http://testzone1.login.10.244.0.34.xip.io/saml/SSO/alias/testzone1.login.10.244.0.34.xip.io',
-    'SingleLogoutService' => 'http://testzone1.login.10.244.0.34.xip.io/saml/SSO/alias/testzone1.login.10.244.0.34.xip.io',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-    'simplesaml.nameidattribute' => 'emailAddress',
-);
-
-$metadata['testzone1.cloudfoundry-saml-login'] = array(
-    'AssertionConsumerService' => 'http://testzone1.localhost:8080/uaa/saml/SSO/alias/testzone1.cloudfoundry-saml-login',
-    'SingleLogoutService' => 'http://testzone1.localhost:8080/uaa/saml/SSO/alias/testzone1.cloudfoundry-saml-login',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-    'simplesaml.nameidattribute' => 'emailAddress',
-);
-
-$metadata['testzone2.cloudfoundry-saml-login'] = array(
-    'AssertionConsumerService' => 'http://testzone2.localhost:8080/uaa/saml/SSO/alias/testzone2.cloudfoundry-saml-login',
-    'SingleLogoutService' => 'http://testzone2.localhost:8080/uaa/saml/SSO/alias/testzone2.cloudfoundry-saml-login',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-    'simplesaml.nameidattribute' => 'emailAddress',
-);
-
-$metadata['testzone3.cloudfoundry-saml-login'] = array(
-    'AssertionConsumerService' => 'http://testzone3.localhost:8080/uaa/saml/SSO/alias/invalid',
-    'SingleLogoutService' => 'http://testzone3.localhost:8080/uaa/saml/SSO/alias/invalid',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-    'simplesaml.nameidattribute' => 'emailAddress',
-);
-
-$metadata['testzone4.cloudfoundry-saml-login'] = array(
-    'AssertionConsumerService' => 'http://testzone4.localhost:8080/uaa/saml/SSO/alias/testzone4.cloudfoundry-saml-login',
-    'SingleLogoutService' => 'http://testzone4.localhost:8080/uaa/saml/SSO/alias/testzone4.cloudfoundry-saml-login',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
-    'simplesaml.nameidattribute' => 'uid',
-);
-
-$metadata['oidcloginit.cloudfoundry-saml-login'] = array(
-    'AssertionConsumerService' => 'http://oidcloginit.localhost:8080/uaa/saml/SSO/alias/oidcloginit.cloudfoundry-saml-login',
-    'SingleLogoutService' => 'http://oidcloginit.localhost:8080/uaa/saml/SSO/alias/oidcloginit.cloudfoundry-saml-login',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
-    'simplesaml.nameidattribute' => 'uid',
-);
-
-
-$metadata['https://oidc10.identity.cf-app.com'] = array(
-    'AssertionConsumerService' => 'https://oidc10.identity.cf-app.com/saml/SSO/alias/oidc10.identity.cf-app.com',
-    'SingleLogoutService' => 'https://oidc10.identity.cf-app.com/saml/SSO/alias/oidc10.identity.cf-app.com',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-    'simplesaml.nameidattribute' => 'emailAddress',
-);
-
-$metadata['spring.security.saml.sp.id'] = array(
-    'AssertionConsumerService' => 'http://localhost:8080/sample-sp/saml/sp/SSO',
-    'SingleLogoutService' => 'http://localhost:8080/sample-sp/saml/sp/logout',
-    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-    'simplesaml.nameidattribute' => 'emailAddress',
-    'certData' => 'MIICgTCCAeoCCQCuVzyqFgMSyDANBgkqhkiG9w0BAQsFADCBhDELMAkGA1UEBhMC
-VVMxEzARBgNVBAgMCldhc2hpbmd0b24xEjAQBgNVBAcMCVZhbmNvdXZlcjEdMBsG
-A1UECgwUU3ByaW5nIFNlY3VyaXR5IFNBTUwxCzAJBgNVBAsMAnNwMSAwHgYDVQQD
-DBdzcC5zcHJpbmcuc2VjdXJpdHkuc2FtbDAeFw0xODA1MTQxNDMwNDRaFw0yODA1
-MTExNDMwNDRaMIGEMQswCQYDVQQGEwJVUzETMBEGA1UECAwKV2FzaGluZ3RvbjES
-MBAGA1UEBwwJVmFuY291dmVyMR0wGwYDVQQKDBRTcHJpbmcgU2VjdXJpdHkgU0FN
-TDELMAkGA1UECwwCc3AxIDAeBgNVBAMMF3NwLnNwcmluZy5zZWN1cml0eS5zYW1s
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDRu7/EI0BlNzMEBFVAcbx+lLos
-vzIWU+01dGTY8gBdhMQNYKZ92lMceo2CuVJ66cUURPym3i7nGGzoSnAxAre+0YIM
-+U0razrWtAUE735bkcqELZkOTZLelaoOztmWqRbe5OuEmpewH7cx+kNgcVjdctOG
-y3Q6x+I4qakY/9qhBQIDAQABMA0GCSqGSIb3DQEBCwUAA4GBAAeViTvHOyQopWEi
-XOfI2Z9eukwrSknDwq/zscR0YxwwqDBMt/QdAODfSwAfnciiYLkmEjlozWRtOeN+
-qK7UFgP1bRl5qksrYX5S0z2iGJh0GvonLUt3e20Ssfl5tTEDDnAEUMLfBkyaxEHD
-RZ/nbTJ7VTeZOSyRoVn5XHhpuJ0B',
-    'assertion.encryption' => TRUE,
-    'nameid.encryption' => TRUE,
-);
-
-
-/*
- * This example shows an example config that works with Google Apps for education.
- * What is important is that you have an attribute in your IdP that maps to the local part of the email address
- * at Google Apps. In example, if your google account is foo.com, and you have a user that has an email john@foo.com, then you
- * must set the simplesaml.nameidattribute to be the name of an attribute that for this user has the value of 'john'.
- */
-$metadata['google.com'] = array(
-	'AssertionConsumerService' => 'https://www.google.com/a/g.feide.no/acs',
-	'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-	'simplesaml.nameidattribute' => 'uid',
-	'simplesaml.attributes' => FALSE,
-);
