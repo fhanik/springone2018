0d2eafb8853eb864ee1865647b25b4bddbaa20bb
Filip Hanik <fhanik@pivotal.io>
2018-09-18 17:37:15 -0700

    Only build sub projects. Not the root

--- saml/saml-idp-sp/src/main/java/demo/springone2018/saml/SamlIdpSpApplication.java
+++ saml/saml-idp-sp/src/main/java/demo/springone2018/saml/SamlIdpSpApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SamlSpIdpApplication {
+public class SamlIdpSpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SamlSpIdpApplication.class, args);
+		SpringApplication.run(SamlIdpSpApplication.class, args);
 	}
 }
--- saml/saml-idp-sp/src/test/java/demo/springone2018/saml/SamlIdpSpApplicationTests.java
+++ saml/saml-idp-sp/src/test/java/demo/springone2018/saml/SamlIdpSpApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SamlSpIdpApplicationTests {
+public class SamlIdpSpApplicationTests {
 
 	@Test
 	public void contextLoads() {
